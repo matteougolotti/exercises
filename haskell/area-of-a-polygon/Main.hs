407cb2a17e93088f0465cca50bd4c115188d73b9
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2018-11-18 20:45:01 +0100

    Added problem

---
+++ haskell/area-of-a-polygon/Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Main where
+
+import           System.IO
+
+toDouble ∷ String → Double
+toDouble s = read s :: Double
+
+(∑) = sum
+
+area ∷ [(Double, Double)] → Double
+area points = do
+    let coords = unzip points
+    let n = length points - 1
+    let x = fst coords
+    let y = snd coords
+    let first  = (∑) [ x!!i * y!!(i + 1) | i ← [ 0 .. n - 1 ] ] + (x!!n * y!!0)
+    let second = (∑) [ x!!(i + 1) * y!!i | i ← [ 0 .. n - 1 ] ] + (x!!0 * y!!n)
+    (1 / 2) * abs (first - second)
+
+main ∷ IO ()
+main = do
+  c ← getContents
+  let l = fmap words (tail (lines c))
+  let points = fmap (\ [x, y] → (toDouble x, toDouble y)) l
+  print (area points)
