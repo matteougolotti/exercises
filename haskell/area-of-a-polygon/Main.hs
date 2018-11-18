6d0c4db4836db9d1c740bea91c418fe75b422d8d
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2018-11-18 20:45:43 +0100

    Added problem

--- haskell/area-of-a-polygon/Main.hs
+++ haskell/area-of-a-polygon/Main.hs
@@ -1,3 +1,5 @@
+-- | https://www.hackerrank.com/challenges/lambda-march-compute-the-area-of-a-polygon/problem
+
 {-# LANGUAGE UnicodeSyntax #-}
 
 module Main where
@@ -7,16 +9,14 @@ import           System.IO
 toDouble ∷ String → Double
 toDouble s = read s :: Double
 
-(∑) = sum
-
 area ∷ [(Double, Double)] → Double
 area points = do
     let coords = unzip points
     let n = length points - 1
     let x = fst coords
     let y = snd coords
-    let first  = (∑) [ x!!i * y!!(i + 1) | i ← [ 0 .. n - 1 ] ] + (x!!n * y!!0)
-    let second = (∑) [ x!!(i + 1) * y!!i | i ← [ 0 .. n - 1 ] ] + (x!!0 * y!!n)
+    let first  = sum [ x!!i * y!!(i + 1) | i ← [ 0 .. n - 1 ] ] + (x!!n * y!!0)
+    let second = sum [ x!!(i + 1) * y!!i | i ← [ 0 .. n - 1 ] ] + (x!!0 * y!!n)
     (1 / 2) * abs (first - second)
 
 main ∷ IO ()
