c2f44bb0a6cc1f96faa8943b43b3a30da04d960c
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2018-12-06 20:53:17 +0100

    Added new exercise

--- ctci/chapter_9/one/src/Main.hs
+++ ctci/chapter_9/one/src/Main.hs
@@ -6,20 +6,33 @@ module Main where
 
   import qualified Control.Monad.State.Strict as ST
   import qualified Data.Map.Strict            as M
+  import Data.Maybe
 
-  set :: Int -> State (M.Map Int Int) ()
-  set n = state (\ st -> ((), M.insert n n st))
+  set :: Int → ST.State (M.Map Int Int) ()
+  set n = ST.state (\ st → ((), M.insert n n st))
 
-  get :: Int -> State (M.Map Int Int) (Maybe Int)
-  get k = state (\ st -> (M.lookup k st, st))
+  get :: Int → ST.State (M.Map Int Int) (Maybe Int)
+  get k = ST.state (\ st → (M.lookup k st, st))
 
-  count ∷ Int → Int
-  count n
-    | n == 1 = 1
-    | n == 2 = 2
-    | n == 3 = 3
-    | n <= 0 = 0
-    | otherwise = count (n - 1) + count (n - 2) + count (n - 3)
+  count :: Int -> ST.State (M.Map Int Int) Int
+  count n = countWithCache n Nothing
+    where
+      countWithCache ∷ Int → Maybe Int → ST.State (M.Map Int Int) Int
+      countWithCache n c
+        | n == 1    = return 1
+        | n == 2    = return 2
+        | n == 3    = return 3
+        | n <= 0    = return 0
+        | otherwise = case c of
+                        Just x  → return x
+                        Nothing → do
+                          c1 ← get (n - 1)
+                          c2 ← get (n - 2)
+                          c3 ← get (n - 3)
+                          n1 ← countWithCache (n - 1) c1 
+                          n2 ← countWithCache (n - 2) c2
+                          n3 ← countWithCache (n - 3) c3
+                          return (n1 + n2 + n3)
 
   main ∷ IO ()
-  main = print (count 10)
+  main = print (fst $ ST.runState (count 10) M.empty)
