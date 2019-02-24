88e117272f62ef5243461ea994a8f492e2e5b0e5
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2019-02-24 16:14:00 +0100

    Added new problem

---
+++ hackerrank/minimum-distances/main.py
@@ -0,0 +1,30 @@
+import unittest
+
+def minimumDistances(a):
+    last_indexes = dict()
+    distances = dict()
+
+    for i in range(len(a)):
+        if a[i] in last_indexes:
+            if a[i] in distances:
+                distances[a[i]] = min(distances[a[i]], i - last_indexes[a[i]])
+            else:
+                distances[a[i]] = i - last_indexes[a[i]]
+        else:
+            last_indexes[a[i]] = i
+
+    distance_values = [ value for key, value in distances.items() ]
+    
+    if len(distance_values) > 0:
+        return min(distance_values)
+    else:
+        return -1
+
+
+class TestSuite1(unittest.TestCase):
+
+    def testCase1(self):
+        self.assertEqual(3, minimumDistances([7, 1, 3, 4, 1, 7]))
+
+if __name__ == "__main__":
+    unittest.main()
